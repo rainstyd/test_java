8810d7675b804102058d5ac6334cb50acfb033c2
rainstyd <rainstyd@sina.com>
2024-11-09 16:01:30 +0800

    update GetMysqlToMysql class

--- src/main/java/flink/datastream/GetMysqlToMysql.java
+++ src/main/java/flink/datastream/GetMysqlToMysql.java
@@ -21,6 +21,7 @@ import java.sql.PreparedStatement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 public class GetMysqlToMysql {
     public static void main(String[] args) throws Exception {
 
